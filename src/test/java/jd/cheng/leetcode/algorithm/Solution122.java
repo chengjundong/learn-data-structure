8faa76e4c07a1dcc4e28557ffbfe9455e32860a0
Jared <chengjundong@gmail.com>
2020-05-07 05:55:12 +0800

    jucheng, 5/7/2020, leet code

---
+++ src/test/java/jd/cheng/leetcode/algorithm/Solution122.java
@@ -0,0 +1,32 @@
+package jd.cheng.leetcode.algorithm;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+/**
+ * @author jucheng
+ * @see <a href="https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/">best time to buy&sell stock</a>
+ */
+public class Solution122 {
+
+  public int maxProfit(int[] prices) {
+    int min = Integer.MAX_VALUE;
+    int result = 0;
+
+    for(int i=0; i<prices.length; i++) {
+      if(prices[i] < min) {
+        min = prices[i];
+      } else if(prices[i] - min > result) {
+        result = prices[i] - min;
+      }
+    }
+
+    return result;
+  }
+
+  @Test
+  public void test() throws Exception {
+    assertThat(maxProfit(new int[]{7,1,5,3,6,4})).isEqualTo(5);
+  }
+}
---
+++ src/test/resources/SQL/620.sql
@@ -0,0 +1,5 @@
+-- https://leetcode-cn.com/problems/not-boring-movies/
+SELECT * FROM cinema
+WHERE description <> 'boring'
+    AND MOD(id, 2) = 1
+ORDER BY rating DESC;
