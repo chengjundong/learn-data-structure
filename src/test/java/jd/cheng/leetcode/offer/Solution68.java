e5f31f3d3bee75dbcdfb0d6f7d053e90062250ad
Jared <chengjundong@gmail.com>
2020-06-17 05:59:51 +0800

    jucheng, 6/17/2020, leetcode

--- src/test/java/jd/cheng/leetcode/offer/Solution68.java
+++ src/test/java/jd/cheng/leetcode/offer/Solution68.java
@@ -47,6 +47,39 @@ public class Solution68 {
     }
   }
 
+  public TreeNode _lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    List<TreeNode> listP = new ArrayList<>();
+    List<TreeNode> listQ = new ArrayList<>();
+
+    this._recur(root, p, listP);
+    this._recur(root, q, listQ);
+
+    TreeNode result = root;
+    for(int i=listP.size()-1, j=listQ.size()-1; i >=0 && j >= 0; i--,j--) {
+      if(listP.get(i) == listQ.get(j)) {
+        result= listP.get(i);
+      } else {
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  private boolean _recur(TreeNode n, TreeNode p, List<TreeNode> list) {
+    if(n == null) return false;
+    if(n == p) {
+      list.add(n);
+      return true;
+    }
+    boolean found = _recur(n.left, p, list) || _recur(n.right, p, list);
+    if(found) {
+      list.add(n);
+    }
+    return found;
+  }
+
+
   @Test
   public void test() throws Exception {
     TreeNode n = new TreeNode(6);
@@ -57,5 +90,18 @@ public class Solution68 {
 
     assertThat(this.lowestCommonAncestor(n, n.left, n.right)).isEqualTo(n);
     assertThat(this.lowestCommonAncestor(n, n.left, n.left.right)).isEqualTo(n.left);
+
+
+    TreeNode x = new TreeNode(3);
+    x.left = new TreeNode(5);
+    x.right = new TreeNode(1);
+    x.left.right = new TreeNode(2);
+    x.left.left = new TreeNode(6);
+    x.right.left = new TreeNode(8);
+
+    assertThat(this._lowestCommonAncestor(n, n.left, n.right)).isEqualTo(n);
+    assertThat(this._lowestCommonAncestor(n, n.left, n.left.right)).isEqualTo(n.left);
+    assertThat(this._lowestCommonAncestor(x, x.left, x.left.right)).isEqualTo(x.left);
+    assertThat(this._lowestCommonAncestor(x, x.left.left, x.left.right)).isEqualTo(x.left);
   }
 }
